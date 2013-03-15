eab7c6d7c6c05d1f4eee1f498199a4770f9886c3
Michael Wright <michael@wserver.co.uk>
2013-03-15 15:39:43 +0000

    Merge Zack. Remove $this->key, use $key

--- TinyPNG.php
+++ TinyPNG.php
@@ -1,66 +1,88 @@
 <?php
 /**
- *	TinyPNG API v1
+ *  TinyPNG API v1
  *
- * 	Michael Wright - @michaelw90
+ *  Michael Wright - @michaelw90
  */
 
 
-class TinyPNG {
+class TinyPNG
+{
 
-	private $url = 'http://api.tinypng.org/api/shrink';
+    private $url = 'http://api.tinypng.org/api/shrink';
+    private $curl = null;
+    private $lastResult = null;
 
-	private $key = '';
+    /**
+     * Constructor
+     * @param strong $key API key for all requests
+     */
+    public function __construct($key)
+    {
+        if ($this->curl === null) {
+            $this->curl = curl_init();
+            $curlOpts = array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => $this->url,
+                CURLOPT_USERAGENT => 'TinyPNG PHP API v1',
+                CURLOPT_POST => 1,
+                CURLOPT_USERPWD => 'api:' . $key,
+                CURLOPT_BINARYTRANSFER => 1
+            );
+            curl_setopt_array($this->curl, $curlOpts);
+        }
+    }
 
-	private $curl = NULL;
+    /**
+     * Send image shrink request
+     * @param  string $file path to file to shrink
+     * @return boolean       Is HTTP response 200
+     */
+    public function shrink($file)
+    {
+        curl_setopt($this->getCurl(), CURLOPT_POSTFIELDS, file_get_contents($file));
+        $this->lastResult = curl_exec($this->getCurl());
+        return curl_getinfo($this->getCurl(), CURLINFO_HTTP_CODE) === 200;
+    }
 
-	private $lastResult = '';
+    /**
+     * Return API response object
+     * @return object|exception
+     */
+    public function getResult() {
+        return $this->_getResult();
+    }
 
-	/**
-	 * Constructor
-	 * @param strong $key API key for all requests
-	 */
-	public function __construct($key){
-		$this->key = $key;
-	}
+    /**
+     * Return API response as JSON
+     * @return string|exception
+     */
+    public function getResultJson() {
+        return json_decode($this->_getResult());
+    }
 
-	/**
-	 * Instantiate curl request if not already created
-	 * @return resource 
-	 */
-	private function getCurl(){
-		if(is_null($this->curl)){
-			$this->curl = curl_init();
-			curl_setopt_array($this->curl, array(
-				CURLOPT_RETURNTRANSFER => 1,
-				CURLOPT_URL => $this->url,
-				CURLOPT_USERAGENT => 'TinyPNG PHP API v1',
-				CURLOPT_POST => 1,
-				CURLOPT_USERPWD => 'api:' . $this->key,
-				CURLOPT_BINARYTRANSFER => 1
-			));
-		}
-		return $this->curl;
-	}
+    /**
+     * Return API response object
+     * @return object|exception
+     */
+    protected function _getResult()
+    {
+        if ($this->lastResult === null) {
+            throw new Exception('No current result');
+        }
+        return $this->lastResult;
+    }
 
-	/**
-	 * Send image shrink request
-	 * @param  string $file path to file to shrink
-	 * @return boolean       Is HTTP response 200
-	 */
-	public function shrink($file){
-		curl_setopt($this->getCurl(), CURLOPT_POSTFIELDS, file_get_contents($file));
-		$this->lastResult = curl_exec($this->getCurl());
-		return curl_getinfo($this->getCurl(), CURLINFO_HTTP_CODE) === 200;
-	}
+    /**
+     * Return Curl object
+     * @return object|exception
+     */
+    protected function getCurl()
+    {
+        if ($this->curl === null) {
+            throw new Exception('cURL not yet initialized.');
+        }
 
-	/**
-	 * Return API response
-	 * @param  boolean $object Return as object or JSON string
-	 * @return object|string
-	 */
-	public function getResult($object = FALSE){
-		return ($object? json_decode($this->lastResult) : $this->lastResult);
-	}
-
+        return $this->curl;
+    }
 }
